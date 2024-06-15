39b171eb00557bf6db68e5cd5008341300788155
watercubz <eurysosagarcia@gmail.com>
2024-06-15 12:05:14 -0400

    portfolio v2.0.1 💚🚀

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,166 +1,165 @@
 <template>
-  <div class="header">
-    <h1 class="title">Hi.</h1>
-    <h3 class="name line anim-typewriter">
-      My name is {{ name }} <span class="nick-name">{{ nickname }}.</span>
-    </h3>
-
-    <div class="description-div">
-      <p class="description">
-        I am a software developer with an interest in backend development and with
-        knowledge in
-        <span v-for="(lang, index) in data.Langs" :key="index" class="text-highlight">
-          {{ lang }} 
-        </span>
-        experience in frameworks
-        <span v-for="(framework, index) in data.Frameworks" :key="index" class="text-highlight">
-          {{ framework }} 
-        
-        </span>
-      </p>
-    </div>
-
-    <div class="contact-me">
-      <div class="logo-card">
-        <a v-for="(social, index) in data.Socials" :key="index" class="social" target="_blank" :href="social.url" rel="noopener">
-          {{ social.name }}
-        </a>
+  <div
+    class="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5"
+  >
+    <h2 class="mb-1 text-3xl font-extrabold leading-tight text-gray-900">
+      watercubz
+    </h2>
+    <p class="mb-12 text-lg text-gray-500">
+      software engineer specialized in backend development
+    </p>
+    <div class="w-full">
+      <div class="flex flex-col w-full mb-10 sm:flex-row">
+        <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+          <div class="relative h-full ml-0 mr-0 sm:mr-10">
+            <span
+              class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"
+            ></span>
+            <div
+              class="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg"
+            >
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                  About me
+                </h3>
+              </div>
+              <p
+                class="mt-3 mb-1 text-xs font-medium text-indigo-500 uppercase"
+              >
+                ------------
+              </p>
+              <p class="mb-2 text-gray-600">
+                I am a software developer with an interest in backend
+                development and with knowledge in Javascript experience in
+                frameworks Express Nodejs MongoDB Supabase PostgreSQL
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2">
+          <div class="relative h-full ml-0 md:mr-10">
+            <span
+              class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"
+            ></span>
+            <div
+              class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg"
+            >
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                  Technical Skills
+                </h3>
+              </div>
+              <p
+                class="mt-3 mb-1 text-xs font-medium text-purple-500 uppercase"
+              >
+                ------------
+              </p>
+              <p class="mb-2 text-gray-600">
+               <ul>
+                <li>JavaScript</li>
+                <li>MongoDB</li>
+                <li>PostgreSQL</li>
+                <li>Node.js</li>
+                <li>Git</li>
+                <li>Unix/Linux</li>
+                <li>Nginx</li>
+                <li>Supabase</li>
+                <li>GitHub</li>
+              </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col w-full mb-5 sm:flex-row">
+        <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+          <div class="relative h-full ml-0 mr-0 sm:mr-10">
+            <span
+              class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"
+            ></span>
+            <div
+              class="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg"
+            >
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                  Projects
+                </h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-blue-400 uppercase">
+                ------------
+              </p>
+              <p class="mb-2 text-gray-600">
+                   I've undertaken personal projects, freelancing gigs, and hackathons, significantly advancing my skills. While I've used frontend libraries like Next.js, my expertise lies in backend development. Explore my diverse projects on GitHub.
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+          <div class="relative h-full ml-0 mr-0 sm:mr-10">
+            <span
+              class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-yellow-400 rounded-lg"
+            ></span>
+            <div
+              class="relative h-full p-5 bg-white border-2 border-yellow-400 rounded-lg"
+            >
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                  Contact
+                </h3>
+              </div>
+              <p
+                class="mt-3 mb-1 text-xs font-medium text-yellow-400 uppercase"
+              >
+                ------------
+              </p>
+              <p class="mb-2 text-gray-600">
+            <ul class="space-y-2">
+            <li>
+              <a href="https://github.com/watercubz" target="_blank" class="flex items-center space-x-2">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" class="w-6 h-6">
+                <span>GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a href="www.linkedin.com/in/watercubz" target="_blank" class="flex items-center space-x-2">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" class="w-6 h-6">
+                <span>LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/watercubz" target="_blank" class="flex items-center space-x-2">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" class="w-6 h-6">
+                <span>Instagram</span>
+              </a>
+            </li>
+            </ul>
+             </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2">
+          <div class="relative h-full ml-0 md:mr-10">
+            <span
+              class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"
+            ></span>
+            <div
+              class="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg"
+            >
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                  🚀
+                </h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-green-500 uppercase">
+                ------------
+              </p>
+              <p class="mb-2 text-gray-600">
+               "Crafting elegant solutions one line of code at a time, building tomorrow's innovations today." ❤️
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-
-  data() {
-    return {
-      data: {
-        Socials: [
-          { name: "Github", url: "https://github.com/watercubz" },
-          { name: "Instagram", url: "https://www.instagram.com/watercubz" },
-          { name: "Twitter", url: "https://twitter.com/watercubz" },
-        ],
-        Langs: ["Javascript"],
-        Frameworks: ["Express", "Deno", "Firebase", "Nodejs", "MongoDB", "Supabase", "PostgreSQL"],
-      },
-      name: 'Eury Sosa Garcia',
-      nickname: 'dev'
-    };
-  }
-};
-</script>
-<style>
-  @import url("https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap");
-
-  .header {
-    display: flex;
-    margin: 3rem;
-    flex-direction: column;
-  }
-
-  .title {
-   color:  #111827 ;
-    font-size: 5rem;
-  }
-
-  .name {
-    color:#454545;
-    font-size: 2rem;
-  }
-
-  .nick-name {
-    color: #454545;
-  }
-
-  .description-div {
-    max-width: 900px;
-  }
-
-  .description {
-    color: #454545;
-    font-size: 1.5rem;
-  }
-
-  .text-highlight {
-    color: #454545;
-    font-size: 1.3rem;
-    margin-right: 0.2rem;
-    font-weight: 500;
-  }
-
-  .contact-me {
-    margin-top: 30px;
-  }
-
-  .social {
-    color: #8a817c;
-    text-decoration: none;
-    font-weight: 500;
-    margin: 5px;
-  }
-
-  .social:hover {
-    color: #ca9787;
-    animation: ease-in 1s linear;
-  }
-
-  * {
-    font-family: "Maven Pro", sans-serif;
-    overflow-x: hidden;
-  }
-
-  .line {
-    position: relative;
-    top: 50%;
-    width: 16em;
-    border-right: 2px solid rgba(255, 255, 255, 0.75);
-    font-size: 180%;
-    text-align: left;
-    white-space: nowrap;
-    overflow: hidden;
-    transform: translateY(-50%);
-  }
-
-  .anim-typewriter {
-    animation: typewriter 4s steps(43) 1s 1 normal both,
-      blinkTextCursor 700ms steps(43) infinite normal;
-  }
-
-  @keyframes typewriter {
-    from {
-      width: 0;
-    }
-    to {
-      width: 15.3em;
-    }
-  }
-
-  @keyframes blinkTextCursor {
-    from {
-      border-right-color: rgba(255, 255, 255, 0.75);
-    }
-    to {
-      border-right-color: transparent;
-    }
-  }
-
-
-  @media screen and (max-width: 667px ) {
-    .header {
-      margin: 0.5rem;
-    }
-    .title {
-      font-size: 2rem;
-    }
-    .name {
-      font-size: 1.5rem;
-    }
-    .description {
-      font-size: 1rem;
-    }
-    .text-highlight {
-      font-size: 1rem;
-    }
-  }
-</style>
