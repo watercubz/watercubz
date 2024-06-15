adb88698dd3d080649031eba69396298596ec07f
watercubz <eurysosagarcia@gmail.com>
2024-06-15 12:15:36 -0400

    fix: error when doing the build and uploading to production ✅

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup>
-  import Home from "./components/Home.vue";
-  import DarkMode from "./components/DarkModeToggle.vue"
+import Home from "./components/Home.vue";
 </script>
 
 <template>
-  <DarkMode />
-   <Home />
+  <Home />
 </template>
 
 <style>
-  :global(::selection) {
-    color: #ca9787;
-    background-color: #8a817c;
-  }
+:global(::selection) {
+  color: #ca9787;
+  background-color: #8a817c;
+}
 
-  :global(.text-highlight::selection) {
-    color: #757474;
-    background-color: #ca9787;
-  }
+:global(.text-highlight::selection) {
+  color: #757474;
+  background-color: #ca9787;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -42,7 +42,7 @@
               class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"
             ></span>
             <div
-              class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg"
+            class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg"
             >
               <div class="flex items-center -mt-1">
                 <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
